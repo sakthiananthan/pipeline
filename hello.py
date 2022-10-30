fe453f7b37f989de82f19a6030efa72e6cfa53ed
[Vidyanantha] <[vidyanantha145@gmail.com]>
2022-10-30 13:23:29 +0530

    adding html file

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return "hello from flask"
-
+    return render_template("index.html")
 if __name__=="__main__":
     app.run(host="0.0.0.0")
